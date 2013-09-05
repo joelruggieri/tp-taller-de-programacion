abfc001c55648f2d6e65045083035dada32014e2
jonathan.tierno@gmail.com <jonathan.tierno@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-09-05 22:43:52 +0000

    prueba conflicto

--- trunk/vista/src/vista/controller/zonaDragAndDrop/ZonaJuego.cpp
+++ trunk/vista/src/vista/controller/zonaDragAndDrop/ZonaJuego.cpp
@@ -16,7 +16,10 @@ using namespace std;
 ZonaJuego::ZonaJuego() :
 //		ZonaDragAndDrop(Cuerpo(0, 0, 140, 100,0)) {
 		ZonaDragAndDrop(new Cuadrado()) { //TODO UN CUADRADO INMENSO PONDRIA ACA.
-//	this->zonaTablero = new ZonaTablero(Cuerpo(10, 0, 100, 100, 0));
+
+
+
+	//	this->zonaTablero = new ZonaTablero(Cuerpo(10, 0, 100, 100, 0));
 //	this->zonaCreacion = new ZonaCreacion(Cuerpo(120, 0, 20, 100, 0));
 	this->zonaTablero = new ZonaTablero(new Mapa(),60,60);
 	list <ViewFiguraFactory*> *factories = new list<ViewFiguraFactory*>();
