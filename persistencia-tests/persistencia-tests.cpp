0d1a0a3ea596b3212f0178272285d65bb9c49804
javierchoque21@gmail.com <javierchoque21@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-09-08 03:04:50 +0000

    Pruebas de la biblioteca de persistencia.

---
+++ persistencia-tests/persistencia-tests.cpp
@@ -0,0 +1,14 @@
+/*
+ * persistencia-tests.cpp
+ *
+ *  Created on: 07/09/2013
+ *      Author: javier
+ */
+
+#include "gtest/gtest.h"
+#include "figuras_test.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
