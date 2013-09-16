e31f9aa43b7bad906e596d95763e9afb41bb40f2
javierchoque21@gmail.com <javierchoque21@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-09-16 04:47:23 +0000

    Modelo del Nivel.

---
+++ modelo/src/Nivel.cpp
@@ -0,0 +1,37 @@
+/*
+ * Nivel.cpp
+ *
+ *  Created on: 15/09/2013
+ *      Author: javier
+ */
+
+#include "Nivel.h"
+
+Nivel::Nivel(int num) {
+	numero = num;
+}
+
+Nivel::~Nivel() {
+	// TODO Auto-generated destructor stub
+}
+
+void Nivel::agregar(Figura* figura) {
+	objetos.push_back(figura);
+}
+
+int Nivel::cantidadFiguras() const{
+	return objetos.size();
+}
+
+std::list<Figura*> Nivel::getFiguras() {
+	return objetos;
+}
+
+
+int Nivel::getNumero() const{
+	return numero;
+}
+
+void Nivel::setNumero(int n) {
+	this->numero = n;
+}
