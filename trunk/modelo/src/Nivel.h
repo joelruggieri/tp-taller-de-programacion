6073d23806c9ad43cbfa7bce3e2575106d470d99
javierchoque21@gmail.com <javierchoque21@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-09-16 04:47:23 +0000

    Modelo del Nivel.

---
+++ trunk/modelo/src/Nivel.h
@@ -0,0 +1,28 @@
+/*
+ * Nivel.h
+ *
+ *  Created on: 15/09/2013
+ *      Author: javier
+ */
+
+#ifndef NIVEL_H_
+#define NIVEL_H_
+
+#include <list>
+#include "figura/Figura.h"
+
+class Nivel {
+public:
+	Nivel(int numero);
+	virtual ~Nivel();
+	void agregar(Figura* figura);
+	int cantidadFiguras() const;
+	std::list<Figura*> getFiguras();
+	int getNumero() const;
+	void setNumero(int numero);
+private:
+	std::list<Figura*> objetos;
+	int numero;
+};
+
+#endif /* NIVEL_H_ */
