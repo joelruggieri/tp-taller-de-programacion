45f6b99e005cc6546647cb03ac6b3009c1901445
jonathan.tierno@gmail.com <jonathan.tierno@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-09-24 00:45:32 +0000

    Logging al cargador de texturas

--- vista/src/vista/CargadorDeTextures.cpp
+++ vista/src/vista/CargadorDeTextures.cpp
@@ -10,85 +10,75 @@
 #include <iostream>
 using namespace std;
 
-CargadorDeTextures* CargadorDeTextures::pinstance = 0;// Inicializar el puntero
-CargadorDeTextures* CargadorDeTextures::Instance (SDL_Renderer* render)
-{
-  if (pinstance == 0)  // ¿Es la primera llamada?
-  {
-    pinstance = new CargadorDeTextures(render); // Creamos la instancia
-  }
-  return pinstance; // Retornamos la dirección de la instancia
+CargadorDeTextures* CargadorDeTextures::pinstance = 0; // Inicializar el puntero
+CargadorDeTextures* CargadorDeTextures::Instance(SDL_Renderer* render) {
+	if (pinstance == 0)  // ¿Es la primera llamada?
+			{
+		pinstance = new CargadorDeTextures(render); // Creamos la instancia
+	}
+	return pinstance; // Retornamos la dirección de la instancia
 }
 
-CargadorDeTextures* CargadorDeTextures::Instance ()
-{
+CargadorDeTextures* CargadorDeTextures::Instance() {
 	Logger log;
 
-	if(pinstance == 0){
+	if (pinstance == 0) {
 		//TODO LANZAR EXCEPTION
-		std::string mensaje_error = "No se pudo inicializar la Instancia correctamente";
+		std::string mensaje_error =
+				"No se pudo inicializar la Instancia correctamente";
 		log.error(mensaje_error);
 		//cout<< "ERROR"<<endl;
 	}
-  return pinstance; // Retornamos la dirección de la instancia
+	return pinstance; // Retornamos la dirección de la instancia
 }
-
-
 
 CargadorDeTextures::CargadorDeTextures(SDL_Renderer* render) {
 	this->rutaError = "resource/imagenError.jpg";
 	this->render = render;
+	pair<string, SDL_Texture*> par;
+	SDL_Texture* texture;
+	texture = IMG_LoadTexture(this->render, this->rutaError.c_str());
+	this->Textures.insert(pair<string, SDL_Texture*>(rutaError, texture));
 	return;
 
 }
 
-
-
-
-SDL_Texture* CargadorDeTextures::cargarTexture(string ruta)
-{
+SDL_Texture* CargadorDeTextures::cargarTexture(string ruta) {
 	pair<string, SDL_Texture*> par;
 	SDL_Texture* texture;
+	Logger log;
+	string msj;
 	map<string, SDL_Texture*>::iterator iter = this->Textures.find(ruta);
-	if (iter!= this->Textures.end())	// Si la textura ya ha sido cargada anteriormente
-	{
-//		this->Textures.insert(par(ruta, ))
+	if (iter != this->Textures.end()) {// Si la textura ya ha sido cargada anteriormente la retorno.
+		msj = "La textura '" + ruta + "' ya ha sido cargada, se omite la carga";
+		log.info(msj);
 		return iter->second;
-	}
-	else	//si la textura no esta en el mapa , genero la textura, y la asigno
-	{
-		texture = IMG_LoadTexture(this->render, ruta.c_str() );
-		if (texture == NULL)
-			{
-			texture = IMG_LoadTexture(this->render, this->rutaError.c_str() );
-			this->Textures.insert(pair<string, SDL_Texture*>(rutaError, texture));
-			}
-		else
+	} else {	//si la textura no esta en el mapa , genero la textura, y la asigno
+		texture = IMG_LoadTexture(this->render, ruta.c_str());
+		if (texture == NULL) { // si la imagen no existe asigno la textura de error y retorno esa.
+			map<string, SDL_Texture*>::iterator iter = this->Textures.find(rutaError);
+			this->Textures.insert(pair<string, SDL_Texture*>(ruta, iter->second));
+			msj = "La textura '" + ruta + "' no existe, se retorna la textura por defecto";
+			log.warning(msj);
+			return iter->second;
+		} else
+			log.info("Se ha cargado exitosamente la textura '" + ruta +"'");
 			this->Textures.insert(pair<string, SDL_Texture*>(ruta, texture));
-
-
-
-
+			return texture;
 	}
 
-
-
-return texture; }
-
+}
 
 CargadorDeTextures::~CargadorDeTextures() {
-	map<string, SDL_Texture* >::const_iterator iter;
-	for(iter = this->Textures.begin(); iter != this->Textures.end(); ++iter)
-	{
+	map<string, SDL_Texture*>::const_iterator iter;
+	for (iter = this->Textures.begin(); iter != this->Textures.end(); ++iter) {
 		SDL_DestroyTexture(iter->second);
 
 	}
 	// SDL_DestroyRenderer(this->render); se destruye mas adelante ??
 }
 
-int CargadorDeTextures::getCantTextures()
-{
+int CargadorDeTextures::getCantTextures() {
 	return this->Textures.size();
 }
-
 
