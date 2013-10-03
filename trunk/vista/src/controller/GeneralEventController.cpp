244af09ff2762c5cea58463e6f6ab6378a927cad
jonathan.tierno@gmail.com <jonathan.tierno@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-10-03 21:01:54 +0000

    Algunos retokes

--- trunk/vista/src/controller/GeneralEventController.cpp
+++ trunk/vista/src/controller/GeneralEventController.cpp
@@ -17,7 +17,7 @@
 #include <string>
 #define  MIN_VENTANA_Y 400
 #define MIN_VENTANA_X 400
-#define MAX_VENTANA 700
+#define MAX_VENTANA 900
 #include "../controller/UserEventCreator.h"
 #include "CanvasController.h"
 #include "keyboardEventController/KeyBoardEventController.h"
