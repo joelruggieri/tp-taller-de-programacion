a8e934fe90480291fcbf4a15530d24115713ef9f
javierchoque21@gmail.com <javierchoque21@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-10-15 00:31:23 +0000

    Modelo de la soga

--- trunk/modelo/src/objeto/Soga.h
+++ trunk/modelo/src/objeto/Soga.h
@@ -9,12 +9,19 @@
 #define SOGA_H_
 
 #include "Objeto.h"
+#include <vector>
 
 class Soga: public Objeto {
 public:
 	Soga(float x, float y);
+	Soga(const Soga& figura);
 	virtual ~Soga();
 	bool crearFisicaEstatica(b2World *);
+	void cargar(b2Body* origen, b2Body* destino,  b2World *m_world);
+private:
+	b2Body *crear(b2World *m_world, int x, int y);
+	std::vector<b2Body*> tramos;
+//	b2Body *origen, *destino;
 };
 
 #endif /* SOGA_H_ */
