7bb1105183a815adeb97054b0ba3ba3cea9bf37c
joelruggieri@gmail.com <joelruggieri@gmail.com@0e055b33-9e10-a41d-64d9-2761cfc4d589>
2013-11-04 19:49:37 +0000

    retoque un poco el NivelDao para el tema de leer un nivel y guardar el nivel y le saquè el administradorDeArchivos.

---
+++ persistencia/src/NoSePuedeGuardarNivelException.cpp
@@ -0,0 +1,14 @@
+/*
+ * NoSePuedeGuardarNivelException.cpp
+ *
+ *  Created on: 04/11/2013
+ *      Author: joel
+ */
+
+#include "NoSePuedeGuardarNivelException.h"
+
+NoSePuedeGuardarNivelException::NoSePuedeGuardarNivelException() {
+	// TODO Auto-generated constructor stub
+
+}
+
